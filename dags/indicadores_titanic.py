2aec1139f5fa20453c14c89ff29efc0a35aac66a
kellylyra <kellylyra@gmail.com>
2022-11-12 22:28:19 -0300

    ok

--- dags/indicadores_titanic.py
+++ dags/indicadores_titanic.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import boto3
 
 aws_access_key_id = Variable.get('aws_access_key_id')
-aws_secret_access_key = Variable.get('aws_secret_access_key')
+aws_secret_access_key = Variable.get('aws_secrets_access_key')
 
 client = boto3.client(
     'emr', region_name='us-east-1',
